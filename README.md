4c494197524ebb0652c0fd26058c9389ee4b523e
Lukas Georgieff <lukas.georgieff@bmw.de>
2018-05-22 10:25:53 -0700

    docker: Add mongodb image

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Build the Image
-`sudo docker build --tag http:2.0.0 http`
+- http: `sudo docker build --tag http:3.0.0 http/`
+- mongodb: `sudo docker build --tag mongodb:1.0.0 mongodb/`
 
 # Run the Container
-`sudo docker run --name http --tty --interactive --mount type=bind,source=$(realpath http/sources),target=/docker_ws http:2.0.0`
+- mongodb:
+  - `sudo docker network create docker_ws`
+  - `sudo docker volume create mongodb_data`
+  - `sudo docker volume create mongodb_config`
+  - `sudo docker run --name mongo --detach --mount type=volume,source=mongodb_data,target=/data/db --mount type=volume,source=mongodb_config,target=/data/configdb --publish 127.0.0.1:27017:27017 --net docker_ws mongodb:1.0.0`
+- http: `sudo docker run --name http --tty --interactive --mount type=bind,source=$(realpath http/sources),target=/docker_ws --net docker_ws http:3.0.0`
--- http/sources/main.js
+++ http/sources/main.js
@@ -1,3 +1,94 @@
 'use strict';
 
-console.log("Hello from docker!");
+const MongoClient = require('mongodb').MongoClient;
+
+const DATABASE = 'docker_ws';
+const COLLECTION = 'main';
+const URL = 'mongodb://mongo:27017/';
+
+let db;
+let connection;
+function connect () {
+    const OPTIONS = {useNewUrlParser: true};
+    return new Promise((resolve, reject) => {
+	MongoClient.connect(URL, OPTIONS, function(err, _db) {
+	    if (err) {
+		reject(err);
+	    } else {
+		connection = _db;
+		db = _db.db(DATABASE);
+		resolve(db)
+	    }
+	})
+    })
+	.catch((err) => {
+	    console.error(`Could not connect to ${URL}: ${err}`);
+	    throw err;
+	})
+}
+
+function read_or_create () {
+    return new Promise((resolve, reject) => {
+	db.collection(COLLECTION).findOne({}, function(err, result) {
+	    if (err) {
+		reject(err)
+	    } else if (result == null) {
+		resolve(0);
+	    } else {
+		resolve(result.value);
+	    }
+	});
+    })
+	.then((value) => {
+	    return new Promise((resolve, reject) => {
+		db.collection(COLLECTION).insertOne({value: value}, function(err, res) {
+		    if (err) {
+			reject(err);
+		    } else {
+			resolve(value);
+		    }
+		});
+	    });
+	})
+	.catch((err) => {
+	    console.error("Could not read or create document: ${err}");
+	    throw err;
+	});
+}
+
+function update (current_value) {
+    return new Promise((resolve, reject) => {
+	let new_value = ++current_value;
+	db.collection(COLLECTION).updateOne({}, {$set: {value: new_value}}, function(err, res) {
+	    if (err) {
+		throw err;
+	    } else {
+		resolve(new_value);
+	    }
+	})
+    })
+	.catch((err) => {
+	    console.error(`Could not update the current value: ${err}`);
+	    throw err;
+	});
+}
+
+function print_message (current_value) {
+    console.log(`[${current_value}] Hello from docker!`);
+}
+
+function disconnect () {
+    if (connection) {
+	connection.close();
+    }
+}
+
+connect()
+    .then(read_or_create)
+    .then(update)
+    .then(print_message)
+    .then(disconnect)
+    .catch((err) => {
+	disconnect();
+	console.error(err);
+    });
